333e435f732da30881b0eebcc5c01c891acfb743
Hyowon Kim <87758380+HyowonKim1116@users.noreply.github.com>
2021-12-14 00:32:07 +0900

    Create PS#23811.py

---
+++ PS#23811.py
@@ -0,0 +1,18 @@
+#https://www.acmicpc.net/problem/23811
+
+N = int(input())
+
+for _ in range(N):
+    print('@' * N * 5)
+
+for _ in range(N):
+    print('@' * N)
+
+for _ in range(N):
+    print('@' * N * 5)
+
+for _ in range(N):
+    print('@' * N)
+
+for _ in range(N):
+    print('@' * N * 5)
